c0506fbf635b63635e8f0459553cfbba7f708018
Krystian <Krystian.Dutkait@gmail.com>
2022-02-20 23:05:07 +0100

    Add task1 to chapter1 checkyourskills

---
+++ Chapter1/CheckYourSkills/task1.py
@@ -0,0 +1,7 @@
+# Create and save a program that displays your name and waits for the user to press the Enter key
+
+print("My name")
+
+print("Krystian")
+
+input("\n\nTo end the programme, press the Enter key.")
