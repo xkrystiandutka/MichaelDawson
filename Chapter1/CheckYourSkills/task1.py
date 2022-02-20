ae953d593d65c88139d80998f1531626938140bf
Krystian <Krystian.Dutkait@gmail.com>
2022-02-20 23:13:12 +0100

    Add task2 to chapter1 checkyourskills

--- Chapter1/CheckYourSkills/task1.py
+++ Chapter1/CheckYourSkills/task1.py
@@ -1,7 +1,5 @@
 # Create and save a program that displays your name and waits for the user to press the Enter key
 
-print("My name")
-
-print("Krystian")
+print("My name is Krystian")
 
 input("\n\nTo end the programme, press the Enter key.")
---
+++ Chapter1/CheckYourSkills/task2.py
@@ -0,0 +1,6 @@
+# Write a program that displays your favourite quotation in the next line give the name of the author of the quoted statement.
+
+quote = "Humans aren't made perfectly. Everyone lies. Even so... I've been careful not to tell lies that hurt others."
+print(quote)
+
+print("~Light Yagami")
