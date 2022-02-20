892596fe511f6d119a17e6fed0cc06b650de7ca5
Krystian <Krystian.Dutkait@gmail.com>
2022-02-20 22:56:49 +0100

    End game & switch to visual studio code

---
+++ Chapter1/EndGame/endGame.py
@@ -0,0 +1,7 @@
+# End game
+# first time use print
+
+print("End Game")
+
+input("\n\nTo end the programme, press the Enter key.")
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# My solutions for the Michael Dawson book exercises.
