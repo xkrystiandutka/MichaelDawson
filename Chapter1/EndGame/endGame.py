66ef09bbc9a47d21d8f7ade9ca62422db1de846b
Krystian <Krystian.Dutkait@gmail.com>
2022-02-20 23:29:41 +0100

    Update endGame.py

--- Chapter1/EndGame/endGame.py
+++ Chapter1/EndGame/endGame.py
@@ -1,7 +1,30 @@
-# End game
-# first time use print
+# End game - version 2
+# Demonstrates the use of inverted commas in character strings
 
-print("End Game")
+print("Programme 'End Game' 2.0")
+
+print("Same", "message ", "as before")
+
+print("only",
+      "somewhat",
+      "greater.")
+
+print("Here ", end=" ")
+print("it is...")
+
+print(
+        """
+                                88                                                       
+                                88                                                       
+                                88                                                       
+ ,adPPYba, 8b,dPPYba,   ,adPPYb,88  ,adPPYb,d8 ,adPPYYba, 88,dPYba,,adPYba,   ,adPPYba,  
+a8P_____88 88P'   `"8a a8"    `Y88 a8"    `Y88 ""     `Y8 88P'   "88"    "8a a8P_____88  
+8PP""""""" 88       88 8b       88 8b       88 ,adPPPPP88 88      88      88 8PP"""""""  
+"8b,   ,aa 88       88 "8a,   ,d88 "8a,   ,d88 88,    ,88 88      88      88 "8b,   ,aa  
+ `"Ybbd8"' 88       88  `"8bbdP"Y8  `"YbbdP"Y8 `"8bbdP"Y8 88      88      88  `"Ybbd8"'  
+                                    aa,    ,88                                           
+                                     "Y8bbdP"          
+        """
+     )
 
 input("\n\nTo end the programme, press the Enter key.")
-
