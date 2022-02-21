8fa64d7b8c1024a0cce418f26f8a42b0d3ab2cdd
Krystian <Krystian.Dutkait@gmail.com>
2022-02-21 23:35:12 +0100

    Add greeters & personalGreeter programme

---
+++ Chapter2/greeters.py
@@ -0,0 +1,10 @@
+# Greeters
+# Demonstrates the use of a variable
+
+name = "Krystian"
+
+print(name)
+
+print("Hi,", name)
+
+input("\n\nTo exit the program, press the Enter key.")
---
+++ Chapter2/personalGreeter.py
@@ -0,0 +1,9 @@
+# Personal greeter
+# Demonstrates retrieval of user input
+name = input("Hi. What's your name? ")
+
+print(name)
+
+print("Hi,", name)
+
+input("\n\nTo exit the program, press the Enter key.")
