f3b301c8bdb7376a77a16a571b9490677ba239fc
Krystian <Krystian.Dutkait@gmail.com>
2022-02-21 23:31:11 +0100

    Add textTasks programme

---
+++ Chapter2/textTasks.py
@@ -0,0 +1,30 @@
+# Text tasks
+# Numbers and mathematical operations
+
+print("A pregnant hippopotamus weighing 1500 kg gives birth to a 45-kg cub,")
+print("but then eats 25 kg of feed. What is her new weight?")
+input("To find out, press enter.")
+print("1500 - 45 + 25 =", 1500 - 45 + 25)
+
+print("The adventurer returns from a successful expedition and buys each of his own")
+print("6 accompanies 3 bottles of beer. How many bottles were purchased?")
+input("To find out, press the enter key.")
+print("6 * 3 =", 6 * 3)
+
+print("The total cost of the restaurant dinner is 159 PLN including tip and you")
+print("You decide with your friends to divide it into 4 equal parts. How much")
+print("Each of you will have to pay?")
+input("To find out, press enter.")
+print("159 / 4 =", 159 / 4)
+
+print("A group of 4 pirates finds a chest containing 107 gold coins, and")
+print("They decide to divide their catch equally. How many coins will each of them receive?")
+input("To find out, press the enter key.")
+print("107 // 4 =", 107 // 4)
+
+print("The same group of 4 pirates will divide the 107 coins equally among themselves")
+print("of the coins in the chest you found, how many coins will remain after the division?")
+input("To find out, press the enter key.")
+print("107 % 4 =", 107 % 4)
+
+input("To exit the program, press the Enter key.")
