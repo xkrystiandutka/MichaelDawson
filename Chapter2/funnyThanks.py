ed596b7a41843a9df19af99ff736190ddbe0103a
Krystian <Krystian.Dutkait@gmail.com>
2022-02-21 23:14:14 +0100

    Add funnyThanks programme

---
+++ Chapter2/funnyThanks.py
@@ -0,0 +1,18 @@
+# Funny thanks
+# Demonstruje sekwencje specjalne
+
+print("\t\t\tFunny thanks")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t      wrote")
+print("\t\t\t  Michael Dawson")
+print("\t\t\t  \\ \\ \\ \\ \\ \\ \\")
+
+print("\nSpecial thanks to:")
+print("to my hairdresser,", end=" ")
+print("To Henry the Great, who never says no can do\".")
+
+# system bell
+print("\a")
+
+input("\n\nTo end the program, press enter.")
