dda934fde5e839a63c54cd76ff990016a42e4af3
Krystian <Krystian.Dutkait@gmail.com>
2022-03-05 14:44:01 +0100

    Update task3 to chapter2 checkyourskills

--- Chapter2/CheckYourSkills/task3.py
+++ Chapter2/CheckYourSkills/task3.py
@@ -3,16 +3,19 @@
 # Calculate the tax and registration fee as a percentage of the value of the car and the other charges as a constant and display the actual price of the car.
 
 car = input("How much cost your car? ")
-
 car= int(car)
+
+if car > 50_000:
+    commissionDealer = 2500
+if car < 50_000:
+    commissionDealer = 1000
+importTax = 2500
+
 
 tax = car*0.17
 tax = int(tax)
 registrationFee = car*0.05
 registrationFee = int(registrationFee)
-
-commissionDealer = 1500
-importTax = 2500
 
 cost = car + tax + registrationFee + commissionDealer + importTax
 
