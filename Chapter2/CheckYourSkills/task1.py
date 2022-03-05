b74a484214d06a1b65a9ebb46cec278bf9533a3f
Krystian <Krystian.Dutkait@gmail.com>
2022-03-05 14:13:27 +0100

    Add task1 to chapter2 checkyourskills

---
+++ Chapter2/CheckYourSkills/task1.py
@@ -0,0 +1,12 @@
+# Write a program that allows the user to enter the names of his two favourite flavours.
+# Then the program should write out, by concatenation, the name of the treat formed from the two treats given by the user.
+
+
+
+delicacy = input("Name your favourite delicacy: ")
+delicacy2 = input("Name your secound favourite delicacy: ")
+
+connectDelicacy = delicacy + delicacy2
+
+print("Concatenated two different strings:",connectDelicacy)
+
