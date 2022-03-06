e425458ce95a7522577c6929ebd6049ec97c65d3
Krystian <Krystian.Dutkait@gmail.com>
2022-03-06 21:39:36 +0100

    Add password programme

---
+++ Chapter3/password.py
@@ -0,0 +1,10 @@
+# Password
+print("Welcome to the Safe Computer System")
+print("-- security is the foundation of our operation")
+password = input("Input password: ")
+
+if password == "test":
+    print("Welcome")
+
+
+input("\n\nTo end the programme, press the Enter key.")
