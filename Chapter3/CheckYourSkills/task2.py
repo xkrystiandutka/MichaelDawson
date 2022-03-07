586ea5e263d89e8f9d39be192756045e0e53f767
Krystian <Krystian.Dutkait@gmail.com>
2022-03-07 10:47:18 +0100

    Add task2 to chapter3 checkyourskills

---
+++ Chapter3/CheckYourSkills/task2.py
@@ -0,0 +1,24 @@
+#Write a program that tosses a coin 100 times and then gives the result of eagles and tail
+
+import random
+
+print('\nPrepare for the draw of 100 coins!!! ')
+
+
+eagles = 0
+tail = 0
+tries = 0
+
+while tries < 100:
+    coin = random.randint(1,2)
+    if coin == 1:
+        eagles += 1
+    else:
+        tail += 1
+    tries += 1
+
+print("Drawing results:")
+print("Eagles - ", eagles)
+print("Tail - ", tail)
+print(tries)
+
