11bdb3cbfce63339b8cfd5a7308f9095a62b01cc
Krystian <Krystian.Dutkait@gmail.com>
2022-03-08 16:24:47 +0100

    Add counting programme

---
+++ Chapter4/counter.py
@@ -0,0 +1,17 @@
+# Counter
+
+print("Counting ")
+for i in range(10):
+    print(i, end="")
+
+
+print("\n\nCounting every 5")
+for i in range(0, 50, 5):
+    print(i, end="")
+
+print("\n\nCounting backwards")
+for i in range(10, 0, -1):
+    print(i, end="")
+
+input("\n\nTo exit the program, press enter")
+
