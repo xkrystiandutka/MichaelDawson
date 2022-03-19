7c855fc2d63982d88062f0010b0c06c46e383cfb
Krystian <Krystian.Dutkait@gmail.com>
2022-03-19 20:05:02 +0100

    Finish hangman game

--- Chapter5/Hangman.py
+++ Chapter5/Hangman.py
@@ -7,7 +7,6 @@
 # import modules
 
 import random
-import time
 
 
 hangman = (
@@ -105,3 +104,55 @@ hangman = (
     |___           
     HANGMAN""")
 
+MAX_WRONG = len(hangman) - 1
+WORDS = ("ARTIFICIAL INTELLIGENCE", "MACHINE LEARNING", "API", "PROGRAMMING", "PYTHON")
+
+# initialise variables
+word = random.choice(WORDS) # word to guess
+so_far = "-" * len(word) # a dash replaces a missed letter
+wrong = 0 # number of wrong letters
+used = [] # letters already used in guessing
+
+
+print("Welcome to the game 'Gallows'. Good luck!")
+
+while wrong < MAX_WRONG and so_far != word:
+    print(hangman[wrong])
+    print("You have already used the following letters: used", used)
+    print("So far the puzzle word looks like this:so_far", so_far)
+
+    guess = input("Enter letter: ")
+    guess = guess.upper()
+    
+    while guess in used:
+        print("You have already used a letter", guess)
+        guess = input("Enter letter: ")
+        guess = guess.upper()
+
+    used.append(guess)
+
+    if guess in word:
+        
+
+        # create a new version of the variable so_far to contain the guessed letter
+        new = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]              
+        so_far = new
+
+    else:
+        
+        wrong += 1
+
+if wrong == MAX_WRONG:
+    print(hangman[wrong])
+    print("You've been hanged!")
+else:
+    print("You guessed it!")
+    
+print("Guess word is", word)
+
+input("To exit the program, press enter.")
