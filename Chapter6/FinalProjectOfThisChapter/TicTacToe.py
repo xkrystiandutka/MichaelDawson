05b56d8c16559fca1b41eb2a5c2dbb4105979f27
Krystian <Krystian.Dutkait@gmail.com>
2022-03-21 15:19:22 +0100

    Add function legalMoves&winner
    legalMoves() - creates a list of legal moves. winner() - determines the winner.

--- Chapter6/FinalProjectOfThisChapter/TicTacToe.py
+++ Chapter6/FinalProjectOfThisChapter/TicTacToe.py
@@ -2,10 +2,6 @@
 # Computer playing tic-tac-toe against man
    
 # Global constants
-from queue import Empty
-from urllib import response
-
-
 X = "X"
 O = "O"
 EMPTY = " "
@@ -63,10 +59,37 @@ def newBoard():
         board.append(EMPTY)
     return board
 
-def displayBoard():
+def displayBoard(board):
     print("\n\t", board[0], "|", board[1], "|", board[2])
     print("\t", "---------")
     print("\t", board[3], "|", board[4], "|", board[5])
     print("\t", "---------")
     print("\t", board[6], "|", board[7], "|", board[8], "\n")
 
+def legalMoves(board):
+    moves = []
+    for square in range(NUM_SQUARES):
+        if board[square] == EMPTY:
+            moves.append(square)
+    return moves
+
+
+def winner(board):
+    WAYS_TO_WIN = ((0, 1, 2),
+                   (3, 4, 5),
+                   (6, 7, 8),
+                   (0, 3, 6),
+                   (1, 4, 7),
+                   (2, 5, 8),
+                   (0, 4, 8),
+                   (2, 4, 6))
+    
+    for row in WAYS_TO_WIN:
+        if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
+            winner = board[row[0]]
+            return winner
+
+    if EMPTY not in board:
+        return TIE
+
+    return None
